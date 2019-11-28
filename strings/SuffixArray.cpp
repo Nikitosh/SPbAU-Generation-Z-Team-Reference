578c1f552b89557853097e9c6f3a5c0d82e52490
egor-bogomolov <msicar.ot.on.yas@gmail.com>
2019-11-28 22:10:47 +0300

    Replace mod

--- strings/SuffixArray.cpp
+++ strings/SuffixArray.cpp
@@ -2,8 +2,14 @@ int sLen, num[MAX_N + 1];
 char s[MAX_N + 1];
 int p[MAX_N], col[MAX_N], inv[MAX_N], lcp[MAX_N];
 
-inline int mod(int x) {
-	return (x + sLen) % sLen;
+inline int add(int a, int b) {
+	a += b;
+	return a >= sLen ? a - sLen : a;
+}
+
+inline int sub(int a, int b) {
+	a -= b;
+	return a < 0 ? a + sLen : a;
 }
 
 void buildArray(int n) {
@@ -18,12 +24,12 @@ void buildArray(int n) {
 		forn (i, n) num[col[i] + 1]++;
 		forn (i, ma) num[i + 1] += num[i];
 		forn (i, n) 
-			inv[num[col[mod(p[i] - k)]]++] = mod(p[i] - k);
+			inv[num[col[sub(p[i], k)]]++] = sub(p[i], k);
 		int cc = 0;
 		forn (i, n) {
-			bool add = col[inv[i]] != col[inv[i - 1]];
-			add |= col[mod(inv[i] + k)] != col[mod(inv[i - 1] + k)];
-			if (i && add) cc++;
+			bool flag = col[inv[i]] != col[inv[i - 1]];
+			flag |= col[add(inv[i], k)] != col[add(inv[i - 1], k)];
+			if (i && flag) cc++;
 			num[inv[i]] = cc;
 		}
 		forn (i, n) p[i] = inv[i], col[i] = num[i];
@@ -43,7 +49,7 @@ void buildLCP(int n) {
 		int i = inv[ind];
 		len = max(0, len - 1);
 		if (i != n - 1)
-			while (len < n && s[mod(p[i] + len)] == s[mod(p[i + 1] + len)]) 
+			while (len < n && s[add(p[i], len)] == s[add(p[i + 1], len)]) 
 				len++;
 		lcp[i] = len;
 		if (i != n - 1 && p[i + 1] == n - 1) len = 0;
