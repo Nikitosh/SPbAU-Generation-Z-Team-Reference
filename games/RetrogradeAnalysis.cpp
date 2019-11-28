e6fbcf169516402efc09576de7fc2bb201813d60
egor-bogomolov <msicar.ot.on.yas@gmail.com>
2019-11-28 16:02:40 +0300

    Change to bfs

--- games/RetrogradeAnalysis.cpp
+++ games/RetrogradeAnalysis.cpp
@@ -1,27 +1,28 @@
-vi g[MAX_N]; // reversed edges
-int win[MAX_N], lose[MAX_N], used[MAX_N], deg[MAX_N];
+int win[MAX_N], lose[MAX_N], outDeg[MAX_N];
+vi rg[MAX_N];
 
-void dfs(int v) {
-	used[v] = 1;
-	for (int u : g[v])
-		if (!used[u]) {
-			if (lose[v])
-				win[u] = 1;
-			else if (--deg[u] == 0)
-				lose[u] = 1;
-			else
-				continue;
-			dfs(u);	
+void retro(int n) {
+	queue<int> q;
+	forn(i, n)
+		if (!outDeg[i])
+			lose[i] = 1, q.push(i);
+	while(!q.empty()) {
+		int v = q.front();
+		q.pop();
+		for(int to : rg[v]) {
+			if (lose[v]) {
+				if (!win[to]) {
+					win[to] = 1;
+					q.push(to);
+				}
+			} else {
+				outDeg[to]--;
+				if (!outDeg[to]) {
+					lose[to] = 1;
+					q.push(to);
+				}
+			}
 		}
+	}
 }
 
-void retrogradeAnalysis(int n, vi initLose, vi initWin) {
-	for (int v : initLose)
-		lose[v] = 1;
-	for (int v : initWin)
-		win[v] = 1;
-	forn (i, n)
-		if (!used[i] && (win[i] || lose[i]))
-			dfs(i);
-}
-	
