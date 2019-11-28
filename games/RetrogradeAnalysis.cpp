4872255219dbb327d61c039b4af6d4e32fe39bf1
Nikitosh <npodguzov@yandex.ru>
2019-11-28 14:29:46 +0100

    Fix code style

--- games/RetrogradeAnalysis.cpp
+++ games/RetrogradeAnalysis.cpp
@@ -3,25 +3,20 @@ vi rg[MAX_N];
 
 void retro(int n) {
 	queue<int> q;
-	forn(i, n)
+	forn (i, n)
 		if (!outDeg[i])
 			lose[i] = 1, q.push(i);
-	while(!q.empty()) {
+	while (!q.empty()) {
 		int v = q.front();
 		q.pop();
-		for(int to : rg[v]) {
+		for (int to : rg[v]) 
 			if (lose[v]) {
-				if (!win[to]) {
-					win[to] = 1;
-					q.push(to);
-				}
+				if (!win[to]) 
+					win[to] = 1, q.push(to);			
 			} else {
 				outDeg[to]--;
-				if (!outDeg[to]) {
-					lose[to] = 1;
-					q.push(to);
-				}
+				if (!outDeg[to])
+					lose[to] = 1, q.push(to);
 			}
-		}
 	}
 }
