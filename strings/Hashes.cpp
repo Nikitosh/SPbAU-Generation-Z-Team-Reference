d4ca9f6d585025a3cde31d2b9b2e2276f7bf09f2
Nikita Podguzov <npodguzov@yandex.ru>
2020-05-07 14:41:10 +0200

    Update Hashes.cpp

--- strings/Hashes.cpp
+++ strings/Hashes.cpp
@@ -1,4 +1,4 @@
-const int P = 239017;
+const int P = 239017, MOD_X = 1e9 + 7, MOD_Y = 1e9 + 9;
 
 inline int add(int a, int b, int m) {
 	a += b;
@@ -9,32 +9,30 @@ inline int sub(int a, int b, int m) {
 	a -= b;
 	return a < 0 ? a + m : a;
 }
-         
-const int MOD_X = 1e9 + 9, MOD_Y = 1e9 + 7;
+
+inline int mul(int a, int b, int m) {
+	return (a * 1ll * b) % m;
+}
 
 // using H = unsigned long long;
 struct H {
 	int x, y;
-	H(): x(0), y(0) {}
+	H() = default;
 	H(int _x): x(_x), y(_x) {}
 	H(int _x, int _y): x(_x), y(_y) {}
 	inline H operator+(const H& h) const { return H(add(x, h.x, MOD_X), add(y, h.y, MOD_Y)); }
 	inline H operator-(const H& h) const { return H(sub(x, h.x, MOD_X), sub(y, h.y, MOD_Y)); }
-	inline H operator*(ll k) const { return H(int((x * k) % MOD_X), int((y * k) % MOD_Y)); }
-	inline H operator*(const H& h) const{ return H(int((ll(x) * h.x) % MOD_X), int((ll(y) * h.y) % MOD_Y)); }
+	inline H operator*(const H& h) const { return H(mul(x, h.x, MOD_X), mul(y, h.y, MOD_Y)); }
 	inline bool operator==(const H& h) const { return x == h.x && y == h.y; }
-	inline bool operator!=(const H& h) const { return x != h.x || y != h.y; }
-	inline bool operator<(const H& h) const { return x < h.x || (x == h.x && y < h.y); }
-	explicit inline operator ll() const { return ll(x) * MOD_Y + y + 1; } // > 0
 };
 
-H deg[MAX_N], h[MAX_N];
+H p[N], h[N];
 
-inline H get(int l, int r) { return h[r] - h[l] * deg[r - l]; }
+inline H get(int l, int r) { return h[r] - h[l] * p[r - l]; }
 
 void init(const string& s) {
 	int n = sz(s);
 	deg[0] = 1;
 	forn (i, n)
-		h[i + 1] = h[i] * P + s[i], deg[i + 1] = deg[i] * P;
+		h[i + 1] = h[i] * P + s[i], p[i + 1] = p[i] * P;
 }
